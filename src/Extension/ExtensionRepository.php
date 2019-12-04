2af423ca12b35c37254c768e2abea7964ae44548
Paul Mitchum <paul@mile23.com>
2019-12-04 12:18:26 -0800

    34

--- src/Extension/ExtensionRepository.php
+++ src/Extension/ExtensionRepository.php
@@ -176,6 +176,10 @@ class ExtensionRepository {
         if (isset($info['package']) && strtolower($info['package']) == 'testing') {
           return FALSE;
         }
+        // Ensure there's a name and type, because some contrib modules have files
+        // matching *.info.yml which are not actually extension metadata. See
+        // date:date_api module for example.
+        return isset($info['name']) && isset($info['type']);
       });
     return $finder;
   }
